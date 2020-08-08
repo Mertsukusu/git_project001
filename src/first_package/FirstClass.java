82403663d0e589b7b203d2592dc97b048367811c
Mert Sukusu <mertsukusu@gmail.com>
2020-08-08 16:06:03 -0400

    the third commit from local master

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -4,7 +4,9 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Our first commit from local repo");
 		System.out.println("Our second commit from local repository");
-		
+		System.out.println("Now I am on my sukusu branch");
+		System.out.println("Now I am on my settar branch");
+		System.out.println("feature branch next commit");
+		System.out.println("");
 	}
-
 }
