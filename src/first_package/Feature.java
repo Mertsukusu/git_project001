63e76f7a4c94619892f1b095b3851ba49e0fa490
mert_ <mertsukusu@gmail.com>
2020-10-08 22:00:20 -0400

    delete a line

--- src/first_package/Feature.java
+++ src/first_package/Feature.java
@@ -4,12 +4,7 @@ public class Feature {
 
 	public static void main(String[] args) {
 		System.out.println("This is my commit from feature branch");
-		System.out.println("This is the first commit from remote master branch");
-		System.out.println("This is the second commit from remote master branch");
-		
-		System.out.println("This is the third commit from local master branch");
-		
-		System.out.println("This is the third commit from remote master branch");
+		System.out.println("we have deleted these line adn add only one");
 	}
 
 }
