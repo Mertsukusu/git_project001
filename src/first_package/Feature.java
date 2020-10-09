ac813b563666e429b2cd0c2c0127b3069e81614a
mert_ <mertsukusu@gmail.com>
2020-10-08 21:58:10 -0400

    delete and add

--- src/first_package/Feature.java
+++ src/first_package/Feature.java
@@ -6,8 +6,7 @@ public class Feature {
 
 	public static void main(String[] args) {
 		System.out.println("This is my commit from feature branch");
-		System.out.println("This is the first commit from remote master branch");
-		System.out.println("This is the second commit from remote master branch");
+		System.out.println("we removed line and add a new line");
 		
 		System.out.println("This is the third commit from local master branch");
 		
